25e85d03a6e7dd9b3f8a7e36fe6dfc637ad8e8d0
NikitBan <nikit.ban2015@gmail.com>
2024-08-13 23:00:06 +0530

    feat header, testimonial

--- src/app/Services/page.js
+++ src/app/Services/page.js
@@ -1,3 +1,11 @@
 export default function Services() {
-  return <p>Welcome to services.....</p>;
+  return(
+    <>
+      <p>Welcome to services.....</p>
+      <div className="w-[300px] h-[300px] bg-green-700 border-solid border-[1.5px] border-green-700 rounded-full pl-[40px] pt-[100px]">
+          hello i am nikit ban
+      </div>
+    </>
+
+  ) ;
 }
---
+++ src/components/Testimonial.js
@@ -0,0 +1,12 @@
+import Image from "next/image"
+export default function Testimonial(){
+    return(
+        <div >
+            <div className="w-[300px] h-[300px] bg-green-700 border-solid border-[1.5px] border-green-700 rounded-full pl-[40px] pt-[100px] mt[100]">
+           
+      </div>
+
+        </div>
+    )
+}
+
