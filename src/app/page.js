a5163b8b2758a033f9e2fc4820a82be331e9a384
NikitBan <nikit.ban2015@gmail.com>
2024-09-03 23:11:07 +0530

    feat:Header Responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import Footer from "@/components/footer/Footer";
-
+import './header.css';
 export default function Home() {
   return(
- <div>
-  <h1 className="text-green-400 text-8xl font-bold mt-10 ml-40" >Operational 
+ <div className="big-text ">
+  <h1 className=" text-green-400 text-8xl font-bold mt-10 ml-40  " >Operational 
    </h1>
-   <div>
+   <span className="text-green-400 text-8xl font-bold mt-10 ml-40 ">Efficiency</span> 
+   <div className="big-text ">
       <h1 className=" font-bold ml-40">
-        <span className="text-green-400 text-8xl">Efficiency</span> 
-        <span className=" text-gray-500 text-5xl"> at</span>
+        
+        <span className="text-gray-500 text-5xl "> at</span>
       </h1>
       <h1 className="text-gray-500 text-5xl font-bold ml-40">its finest. 
       </h1>
