b192f8e3922b90a560c3cd8247ab592c7099d42d
Aaryan Shrestha <133579553+ShresthaAaryan@users.noreply.github.com>
2024-09-28 17:55:40 +0545

    chores

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Four Leaf Clover",
-  description: "",
+  description:
+    "Four Leaf Clover Pvt. Ltd. is a business process outsourcing company that provides its clients with accounting, email and social media marketing, lead generation and conversion, content creation, and client servicing. Education organizations, schools, and universities may save expenses, offer superior customer service, and free up resources to concentrate on more important tasks by streamlining their operations. As a BPO service provider, the business closely matches its values with that of its major customer before taking on the task of providing those values to the client's client. Stated otherwise, Four Leaf Clover acts as a business entity's representative when interacting with the client's consumers, who are individual natural persons.",
 };
 
 export default function RootLayout({ children }) {
