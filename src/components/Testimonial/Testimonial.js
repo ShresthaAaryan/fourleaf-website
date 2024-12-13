d6e2f0d699c44ecbbdf86a94f20e285f670683d4
Aaryan Shrestha <133579553+ShresthaAaryan@users.noreply.github.com>
2024-12-13 20:37:17 +0530

    ui changes

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -16,18 +16,18 @@ export default function Testimonial() {
                 </div>
                 <div className="flex-4 texttestimonial flex flex-col w-full lg:w-[900px] text-center lg:text-left mt-6 lg:mt-0 p-4">
                     <p className="text-base lg:text-lg">
-                    I am truly impressed by Four Leaf Clover's young and enthusiastic team. Their dynamic energy, innovative ideas, and adaptability set them apart. They bring a fresh perspective to every task, leveraging modern tools and fostering strong client relationships. Their passion for excellence and future-oriented vision make them a reliable and exceptional partner. I highly recommend them.
+                        I am truly impressed by Four Leaf Clover&apos;s young and enthusiastic team. Their dynamic energy, innovative ideas, and adaptability set them apart. They bring a fresh perspective to every task, leveraging modern tools and fostering strong client relationships. Their passion for excellence and future-oriented vision make them a reliable and exceptional partner. I highly recommend them.
                         <br />
                     </p>
                     <h4 className="block mt-4 lg:text-lg text-right font-bold lg:text-center justify-center">
-                    Bijaya Gurung, Company Name
+                        Bijaya Gurung, Company Name
                     </h4>
                 </div>
             </div>
             <div className="flex flex-col-reverse lg:flex-row items-center lg:space-x-12 justify-center p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
                 <div className="flex-4 texttestimonial flex flex-col w-full lg:w-[900px] text-center lg:text-left mt-6 lg:mt-0 p-4">
                     <p className="text-base lg:text-lg">
-                    Working with Four Leaf Clover was an absolute pleasure, even with language barriers and time zone differences. The team’s professionalism, clear communication, and adaptability made the entire process seamless. Their commitment to understanding my needs and ensuring everything was handled efficiently truly stood out. I highly recommend Four Leaf Clover for their dedication to making the experience easy and stress-free.
+                        Working with Four Leaf Clover was an absolute pleasure, even with language barriers and time zone differences. The team’s professionalism, clear communication, and adaptability made the entire process seamless. Their commitment to understanding my needs and ensuring everything was handled efficiently truly stood out. I highly recommend Four Leaf Clover for their dedication to making the experience easy and stress-free.
                         <br />
                     </p>
                     <h4 className="block mt-4 lg:text-lg font-bold text-right lg:text-center">
